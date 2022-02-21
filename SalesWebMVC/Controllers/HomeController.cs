fefd106c5ed420d1284a591a019053658394934a
Leonardo de Souza <leosouza1392@gmail.com>
2022-02-20 23:00:07 -0300

    First controller and Razor pages tests

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -15,7 +15,8 @@ namespace SalesWebMVC.Controllers
         }
 
         public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course.";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
